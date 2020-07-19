143229645d045e96d231ef0996876d4b7349d675
alex08neo <alex081994@outlook.com>
2020-07-19 16:24:37 +0800

    Add bash script

--- Connect-Database/MongoDB-Connection.py
+++ Connect-Database/MongoDB-Connection.py
@@ -1,10 +1,20 @@
 # A MongoDB Atlas DB cluster has been set up
 from pymongo import MongoClient
 import os
+import json
 
+directory = '../Data/'
 MONGODB_CONNECTION_STRING = os.getenv("MONGODB_CONNECTION_STRING")
-
+print("connecting to mongoDB")
 client = MongoClient(MONGODB_CONNECTION_STRING)
 db = client.get_database('CherryPickerDB')
-records = db.RawDataCollection
-print(records.count_documents({}))  # Should output 0
+venue = db.venue
+
+for filename in os.listdir(directory):
+    if filename.endswith('.json'):
+       print("Seeding ", os.path.splitext(os.path.basename(filename))[0], "into DB")
+       data =[]
+       with open(directory+filename, encoding='utf-8') as write_file:
+           json_data = json.load(write_file)
+       venue.insert_many(json_data)
+
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for py_file in $(find ./Scrapers ! -name '*Class*')
+do
+    python $py_file &
+done
+
+echo "Running scripts in parallel"
+wait 
+echo "Finished scraping"
+
+python ./Connect-Database/MongoDB-Connection.py
